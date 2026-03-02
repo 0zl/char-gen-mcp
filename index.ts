a6b07011a5f82d6e7c68b0725e207b513535ffd3
0zl <zwiefk0@gmail.com>
2026-03-03 01:29:53 +0700

    hasbang

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import { z } from 'zod'
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
